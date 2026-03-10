a054a8541d622eaaa1b0e8e33f8a0a6d7562772d
Abdellah <abdellah.elkoulali@gmx.fr>
2026-03-10 11:38:06 +0100

    product data

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const products = [
   {id: 3, name: "Product 3", price: 55.00, image: "Images/Products/Gucci.png"},
   {id: 4, name: "Product 4", price: 45.00, image: "Images/Products/louis_vuitton.jpg"},
   {id: 5, name: "Product 5", price: 40.00, image: "Images/Products/Off_White.jpg"},
-  {}
+  {id: 6, name: "Product 6", price: 60.00, image: "Images/Products/supreme_t-shirt.jpg"}
 ];
