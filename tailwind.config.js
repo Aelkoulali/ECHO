f8c7c70605971aae194595a5305eaf185c575cf2
Abdellah <abdellah.elkoulali@gmx.fr>
2026-03-31 18:44:38 +0200

    about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,7 @@ module.exports = {
     './index.html',
   ],
   theme: {
-    extend: {
-      keyframes: {
-        slide: {
-          '0%': { transform: 'translateX(0%)' },
-          '33.33%': { transform: 'translateX(-33.33%)' },
-          '66.66%': { transform: 'translateX(-66.66%)' },
-          '100%': { transform: 'translateX(0%)' },
-        },
-      },
-      animation: {
-        slide: 'slide 9s infinite',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
