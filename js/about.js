4ea56fbae6eb8c1ce14a47bb0d4d945b83e7dfee
Abdellah <abdellah.elkoulali@gmx.fr>
2026-06-30 20:46:55 +0200

    Add carousel functionality and mobile menu toggle to about.js

---
+++ js/about.js
@@ -0,0 +1,17 @@
+// Caroussel
+    const carousel = document.getElementById('carousel');
+    let index = 0;
+
+    setInterval(() => {
+      index = (index + 1) % 4; // Assuming there are 4 slides
+      carousel.style.transform = `translateX(-${index * 100}%)`;
+    }, 3000); // Change slide every 3 seconds  
+
+    // Menu btn 
+    const menuBtn = document.getElementById("menu-btn");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    menuBtn.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+      mobileMenu.classList.toggle("flex");
+    });
