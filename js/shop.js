f250e58fe14f871b6c2db4aaf1e08636d9260aa4
Abdellah <abdellah.elkoulali@gmx.fr>
2026-07-04 23:57:42 +0200

    Remove unused product entries from shop.js for cleaner data structure

--- js/shop.js
+++ js/shop.js
@@ -1,10 +1,6 @@
 
 // Product Data
 const products = [
-  {id: 1, name: "Product 1", price: 35.00, image: "Images/Products/air_jordan.jpg"},
-  {id: 2, name: "Product 2", price: 50.00, image: "Images/Products/balenciaga.jpg"},
-  {id: 3, name: "Product 3", price: 55.00, image: "Images/Products/Gucci.png"},
-  {id: 4, name: "Product 4", price: 45.00, image: "Images/Products/louis_vuitton.jpg"},
-  {id: 5, name: "Product 5", price: 40.00, image: "Images/Products/Off_White.jpg"},
+  {id: 1, name: "Product 1", price: 35.00, image: "Images/Products/air_jordan.jpg"},  
   {id: 6, name: "Product 6", price: 60.00, image: "Images/Products/supreme_t-shirt.jpg"}
 ];
