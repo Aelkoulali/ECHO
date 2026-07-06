9b307b798757cb0cb55bf99d0f44cf2f2c119eb8
Abdellah <abdellah.elkoulali@gmx.fr>
2026-07-06 08:56:06 +0200

    Refactor code structure for improved readability and maintainability

--- js/shop.js
+++ js/shop.js
@@ -2,5 +2,11 @@
 // Product Data
 const products = [
   {id: 1, name: "Céline sunglasses", price: 480.00, category: "Accessories", image: "Images/Products/Accessories/Celine_sunglasses.webp"},  
-  {id: 2, name: "Dior sunglasses", price: 450.00, category: "Accessories", image: "Images/Products/Accessories/Dior_sunglasses.webp"}
+  {id: 2, name: "Dior sunglasses", price: 450.00, category: "Accessories", image: "Images/Products/Accessories/Dior_sunglasses.webp"},
+  {id: 3, name: "Dolce&Gabana belt", price: 500.00, category: "Accessories", image: "Images/Products/Accessories/Dolce&Gabana_belt.webp"},
+  {id: 4, name: "Givenchy belt", price: 550.00, category: "Accessories", image: "Images/Products/Accessories/Givenchy_belt.webp"},
+  {id: 5, name: "Givenchy scarf", price: 600.00, category: "Accessories", image: "Images/Products/Accessories/Givenchy_scarf.webp"},
+  {id: 6, name: "Miu Miu hat", price: 650.00, category: "Accessories", image: "Images/Products/Accessories/MiuMiu_hat.webp"},
+  {id: 7, name: "Prada scarf", price: 700.00, category: "Accessories", image: "Images/Products/Accessories/Prada_scarf.webp"},
+  {id: 8, name: "Saint Laurent belt", price: 750.00, category: "Accessories", image: "Images/Products/Accessories/Saint_Laurent_belt.webp"}
 ];
