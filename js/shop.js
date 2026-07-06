4d1cd7202eee035bc90d632e88cf4523b5900e18
Abdellah <abdellah.elkoulali@gmx.fr>
2026-07-06 09:01:17 +0200

    Refactor code structure for improved readability and maintainability

--- js/shop.js
+++ js/shop.js
@@ -8,5 +8,13 @@ const products = [
   {id: 5, name: "Givenchy scarf", price: 600.00, category: "Accessories", image: "Images/Products/Accessories/Givenchy_scarf.webp"},
   {id: 6, name: "Miu Miu hat", price: 650.00, category: "Accessories", image: "Images/Products/Accessories/MiuMiu_hat.webp"},
   {id: 7, name: "Prada scarf", price: 700.00, category: "Accessories", image: "Images/Products/Accessories/Prada_scarf.webp"},
-  {id: 8, name: "Saint Laurent belt", price: 750.00, category: "Accessories", image: "Images/Products/Accessories/Saint_Laurent_belt.webp"}
+  {id: 8, name: "Saint Laurent belt", price: 750.00, category: "Accessories", image: "Images/Products/Accessories/Saint_Laurent_belt.webp"},
+  {id: 9, name: "Blanciaga bag", price: 2000.00, category: "Bags", image: "Images/Products/Bags/Blanciaga_bag.webp"},
+  {id: 10, name: "Bvlgari bag", price: 2500.00, category: "Bags", image: "Images/Products/Bags/Bvlgari_bag.webp"},
+  {id: 11, name: "Dolce&Gabana bag", price: 3000.00, category: "Bags", image: "Images/Products/Bags/Dolce&Gabana_bag.webp"},
+  {id: 12, name: "Givenchy bag", price: 3500.00, category: "Bags", image: "Images/Products/Bags/Givenchy_bag.webp"},
+  {id: 13, name: "Loewe bag", price: 4000.00, category: "Bags", image: "Images/Products/Bags/Loewe_bag.webp"},
+  {id: 14, name: "Miu Miu bag", price: 4500.00, category: "Bags", image: "Images/Products/Bags/Miu_Miu_bag.webp"},
+  {id: 15, name: "Prada bag", price: 5000.00, category: "Bags", image: "Images/Products/Bags/Prada_bag.webp"}
+  {id: 16, name: "Saint Laurent bag", price: 5500.00, category: "Bags", image: "Images/Products/Bags/Saint_Laurent_bag.webp"}
 ];
