2b18ed65ed97a0c6592df8f1a919ae90331e6e2f
Abdellah <abdellah.elkoulali@gmx.fr>
2026-07-12 11:10:47 +0200

    Update product card width and adjust grid padding for improved layout

--- js/shop.js
+++ js/shop.js
@@ -66,7 +66,7 @@ const productGrid = document.getElementById("product-grid");
 
 function createCard(product) {
   return `
-      <div class="bg-white rounded-lg shadow hover:shadow-lg overflow-hidden w-44">
+      <div class="bg-white rounded-lg shadow hover:shadow-lg overflow-hidden w-34 md:w-32 lg:w-40 transition duration-300 ease-in-out transform hover:scale-105">
       <div class="flex justify-end p-3">
         <button class="text-black hover:text-red-700 transition duration-300 ease-in-out">
             <i class="fa-regular fa-heart fa-lg"></i>
