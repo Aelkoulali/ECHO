9b1845d05a8bedce48ba76a26d359f57aea2a251
Abdellah <abdellah.elkoulali@gmx.fr>
2026-09-01 22:06:44 +0200

    Add cartItem variable declaration in cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,5 @@
 // Declare variables
+const cartItem = document.querySelector('.cart-item');
 const itemCountElement = document.getElementById('item-count');
 const subtotalElement = document.getElementById('subtotal');
 const grandTotalElement = document.getElementById('grand-total');
