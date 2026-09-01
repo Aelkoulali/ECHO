27539838eea921bd91d6118b5ef68783d1271bb9
Abdellah <abdellah.elkoulali@gmx.fr>
2026-09-01 22:05:23 +0200

    Add blank line for improved readability in cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,4 @@
+
 const cartItems = document.querySelectorAll('.cart-item');
 const itemCountElement = document.getElementById('item-count');
 const subtotalElement = document.getElementById('subtotal');
