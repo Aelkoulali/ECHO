67b5eff567ea3be5786d6cadd279b7a125f71347
Abdellah <abdellah.elkoulali@gmx.fr>
2026-09-02 13:21:04 +0200

    Refactor updateCart function to include cartItems processing logic

--- js/cart.js
+++ js/cart.js
@@ -13,6 +13,23 @@ function formatCurrency(amount) {
 function updateCart() {
     let totalQuantity =0;
     let subtotal = 0;
+    
+    cartItems.forEach((cartItem) => {
+      const productId = Number(cartItem.dataset.productId);
+
+      const product = products.find((item) => item.id === productId);
+
+      if (!product) return;
+
+      const quantityInput = cartItem.querySelector(".quantity-input");
+      const itemTotalElement = cartItem.querySelector(".item-total");
+
+      let quantity = parseInt(quantityInput.value, 10);
+
+      if (isNaN(quantity) || quantity < 1) {
+        quantity = 1;
+        quantityInput.value = 1;
+      }
 
 
 updateCart(); 
